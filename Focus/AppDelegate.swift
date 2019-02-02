053feb4271a5648ce827f15c3ce955830c70ba3e
wang yashu <wys@zhihu.com>
2019-02-02 19:28:23 +0800

    Initial Commit

---
+++ Focus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Focus
+//
+//  Created by WongYashu on 2/2/19.
+//  Copyright © 2019 yasmina. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
